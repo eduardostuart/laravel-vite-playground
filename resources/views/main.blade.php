e321069f450a8073970b9e419e496740a8b375d2
Eduardo Stuart <stuart.eduardo@gmail.com>
2021-03-22 22:33:27 +0100

    test vite dev/hot reload

---
+++ resources/views/main.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main</title>
+</head>
+<body>
+    <div class="main">
+        <div class="main__warning">this is fast</div>
+    </div>
+    
+    @production
+        @php
+            $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+        @endphp
+        <script type="module" src="/build/{{ $manifest['resources/js/app.js']['file'] }}"></script>
+        <link rel="stylesheet" href="/build/{{ $manifest['resources/js/app.js']['css'][0] }}">
+    @else
+        <script type="module" src="http://localhost:3000/@vite/client"></script>
+        <script type="module" src="http://localhost:3000/resources/js/app.js"></script>
+    @endproduction
+</body>
+</html>
